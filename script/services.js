cc366572870dd71cc827bb778fa976baf760af76
Matthew <matthewkol186@gmail.com>
2016-06-30 11:11:02 -0400

    Fixed typo

--- script/services.js
+++ script/services.js
@@ -108,7 +108,7 @@ angular.module('teletutor.services', ['firebase'])
                 }
                 
                 scope.endCall = function(){
-                    aleer("this happened!");
+                    alert("this happened!");
                     $window.phone.hangup();
                 }
             }
